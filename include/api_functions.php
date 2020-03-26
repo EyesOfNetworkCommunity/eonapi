5acdc76aeb8fcaea78267f78c5607a3a220f07a2
Jeremy Hoarau <jeremy.hoarau@axians.com>
2020-03-26 14:51:41 +0100

    Reverse unwilling modification

--- include/api_functions.php
+++ include/api_functions.php
@@ -60,17 +60,20 @@ function has_empty($array) {
 
 function getUserByUsername( $username ){
     global $database_eonweb;
+    $connect = connexionDB($database_eonweb);
+    $stmt = $connect->prepare("SELECT U.user_id as user_id,U.user_name as user_name,U.user_passwd as user_passwd,U.user_type as user_type,
+    U.user_limitation as user_limitation,R.tab_1 as readonly,R.tab_2 as operator,R.tab_6 as admin
+    FROM users as U left join groups as G on U.group_id = G.group_id left join groupright as R on R.group_id=G.group_id
+    WHERE U.user_name = :username");
+
+    $stmt->bindParam(':username', $username);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+    $stmt=null;
+    $connect=null;
     
-    $usersql = sqlrequest($database_eonweb,
-		"SELECT U.user_id as user_id,U.user_name as user_name,U.user_passwd as user_passwd,U.user_type as user_type,
-		U.user_limitation as user_limitation,R.tab_1 as readonly,R.tab_2 as operator,R.tab_6 as admin
-		FROM users as U left join groups as G on U.group_id = G.group_id left join groupright as R on R.group_id=G.group_id
-		WHERE U.user_name = '".$username."'",
-		false,
-		array((string)$username)
-	);
-    
-    return $usersql;
+   
+    return $result;
 }
 
 /*---HTTP Response---*/
