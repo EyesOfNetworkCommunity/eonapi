6737cd435c5695fe0a841bd0700b53fc67ef1649
Julien1498 <53144809+Julien1498@users.noreply.github.com>
2020-12-03 16:24:28 +0100

    :hammer: Update api_functions sql

--- include/api_functions.php
+++ include/api_functions.php
@@ -58,20 +58,12 @@ function has_empty($array) {
     return false;
 }
 
-function getUserByUsername( $username ){
+function getUserByUsername($username){
     global $database_eonweb;
-    $connect = connexionDB($database_eonweb);
-    $stmt = $connect->prepare("SELECT U.user_id as user_id,U.user_name as user_name,U.user_passwd as user_passwd,U.user_type as user_type,
+    $result = sql($database_eonweb, "SELECT U.user_id as user_id,U.user_name as user_name,U.user_passwd as user_passwd,U.user_type as user_type,
     U.user_limitation as user_limitation,R.tab_1 as readonly,R.tab_2 as operator,R.tab_6 as admin
     FROM users as U left join groups as G on U.group_id = G.group_id left join groupright as R on R.group_id=G.group_id
-    WHERE U.user_name = :username");
-
-    $stmt->bindParam(':username', $username);
-    $stmt->execute();
-    $result = $stmt->fetchAll();
-    $stmt=null;
-    $connect=null;
-    
+    WHERE U.user_name = ?", array($username));
    
     return $result;
 }
